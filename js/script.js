77dabf14d6b605aa5f6300d407998b76111dfe8e
Nicolò <nico.paganelli91@gmail.com>
2020-06-10 18:00:39 +0200

    jQuery dropdown

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 $(document).ready(
   function() {
-    
+
+    // DROPDOWN
+    $('.con-dropdown').mouseenter(
+      function() {
+
+        $('.con-dropdown .dropdown').fadeOut(50);
+        $(this).children('.dropdown').fadeIn(300);
+      }
+    );
+    $('.con-dropdown').mouseleave(
+      function() {
+
+        $(this).children('.dropdown').fadeOut(50);
+      }
+    );
   }
 );
